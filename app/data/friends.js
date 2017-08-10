99e9841600a715354598a8217a8945b585d5ddc1
Mikael Walters <mikael.walters@gmail.com>
2017-08-09 22:05:52 -0500

    fixed bug with search algorithim and added extra users to the friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -15,6 +15,38 @@ var friends = [{
     "4",
     "1"
   ]
+},
+{
+  "name": "JJ Watt",
+  "photo": "https://pbs.twimg.com/profile_images/871390705712680960/Ql0VzJzC.jpg",
+  "scores": [
+    "4",
+    "4",
+    "5",
+    "1",
+    "3",
+    "2",
+    "4",
+    "3",
+    "1",
+    "5"
+  ]
+},
+{
+  "name": "Oprah Winfrey",
+  "photo": "http://cbsnews1.cbsistatic.com/hub/i/r/2017/01/31/9656c1b6-8574-449a-94b9-cc23ecfae035/thumbnail/1200x630/f8ff2f8e466b3556795e54df9899c712/oprah.jpg",
+  "scores": [
+    "5",
+    "1",
+    "5",
+    "2",
+    "2",
+    "1",
+    "4",
+    "3",
+    "2",
+    "5"
+  ]
 }];
 
 
